71a8504243bc6c28e376a5b482f3bda05318fcf9
XTHEREALX <149897908+XTHEREALX@users.noreply.github.com>
2023-11-06 15:46:46 -0300

    Create NewTitleState.hx

---
+++ source/NewTitleState.hx
@@ -0,0 +1,48 @@
+package;
+
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flixel.system.FlxSound;
+
+using StringTools;
+
+class NewTitleState extends MusicBeatState
+{
+  var BG:FlxSprite;
+
+  override function create()
+
+    #if android
+		addVirtualPad(A_B);
+		#end
+
+    Paths.clearStoredMemory();
+		Paths.clearUnusedMemory();
+
+    FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
+
+    var BG:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('mainmenu/background'));
+    BG.updateHitbox
+    BG.screenCenter();
+    add(BG);
+
+  super.create();
+}
+
+override function update();
+{
+  if (controls.ACCEPT)
+			{
+				MusicBeatState.switchState(new MainMenuState());
+      }
+
+  super.update();
+}
