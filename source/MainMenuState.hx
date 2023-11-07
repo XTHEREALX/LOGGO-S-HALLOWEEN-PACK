c7204249ae615dc38536158e6c78ab175eadb96c
XTHEREALX <149897908+XTHEREALX@users.noreply.github.com>
2023-11-07 14:22:10 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -95,13 +95,13 @@ class MainMenuState extends MusicBeatState
 		for (i in 0...optionShit.length)
 		{
 			var offset:Float = 108 - (Math.max(optionShit.length, 4) - 4) * 80;
-			var menuItem:FlxSprite = new FlxSprite(0, (i * 140)  + offset);
-			menuItem.x = 20;
+			var menuItem:FlxSprite = new FlxSprite(20, (i * 140)  + offset);
 			menuItem.frames = Paths.getSparrowAtlas('mainmenu/menu_' + optionShit[i]);
 			menuItem.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
+			menuItem.x = 20;
 			menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
