c8c648c89404092ac8395058c9d9e9a859d30251
XTHEREALX <149897908+XTHEREALX@users.noreply.github.com>
2023-11-09 18:32:10 -0300

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -85,8 +85,8 @@ class OptionsState extends MusicBeatState
 		for (i in 0...options.length)
 		{
 			var optionText:Alphabet = new Alphabet(20, 20, options[i], true);
-			optionText.y += (100 * (i - (options.length / 2))) + 50;
 			optionText.x = 20;
+			optionText.y = 20;
 			grpOptions.add(optionText);
 		}
 
