4fefede93863ef8cb64d59e65f3b5a7ad6838607
XTHEREALX <149897908+XTHEREALX@users.noreply.github.com>
2023-11-12 13:59:55 -0300

    Update NewTitleState.hx

--- source/NewTitleState.hx
+++ source/NewTitleState.hx
@@ -1,55 +1,71 @@
 package;
 
-import flash.text.TextField;
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.input.keyboard.FlxKey;
 import flixel.addons.display.FlxGridOverlay;
 import flixel.addons.display.FlxBackdrop;
-import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.addons.transition.FlxTransitionSprite.GraphicTransTileDiamond;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.addons.transition.TransitionData;
+import haxe.Json;
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
+import options.GraphicsSettingsSubState;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.graphics.frames.FlxFrame;
+import flixel.group.FlxGroup;
+import flixel.input.gamepad.FlxGamepad;
 import flixel.math.FlxMath;
-import flixel.text.FlxText;
-import flixel.util.FlxColor;
-import lime.utils.Assets;
-import flixel.FlxSubState;
+import flixel.math.FlxPoint;
+import flixel.math.FlxRect;
 import flixel.system.FlxSound;
+import flixel.system.ui.FlxSoundTray;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import openfl.Assets;
 
-using StringTools;
+class NewTitleState extends MusicBeatState{
+var starsBG:FlxBackdrop;
+var BG:FlxSprite;
 
-class NewTitleState extends MusicBeatState
-{
-  var bg:FlxSprite;
-  var starsBG:FlxBackdrop;
+override function create(){
 
-  override public function create()
-  {
-    Paths.clearStoredMemory();
-    Paths.clearUnusedMemory();
+	Paths.clearStoredMemory();
+	Paths.clearUnusedMemory();
+	
+  starsBG = new FlxBackdrop(Paths.image('starsBG'), 1, 1, true, true);
+	starsBG.setPosition(111.3, 67.95);
+  starsBG.antialiasing = true;
+  starsBG.updateHitbox();
+  starsBG.scrollFactor.set();
+  add(starsBG);
 
-    starsBG = new FlxBackdrop(Paths.image('starsBG'), 1, 1, true, true);
-    starsBG.setPosition(111.3, 67.95);
-    starsBG.updateHitbox();
-    starsBG.scrollFactor.set();
-    add(starsBG);
+	BG = new FlxSprite().loadGraphic(Paths.image('TitleScreen'));
+	BG.screenCenter();
+	add(BG);
 
-    var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('TitleScreen'));
-    bg.updateHitbox();
-    bg.screenCenter();
-    add(BG);
-
-    #if android
-    addVirtualPad(A_B);
-    #end
-
-    FlxG.sound.playMusic(Paths.music('freakyMenu'), 0.7);
+	#if android
+  addVirtualPad(FULL, A_B);
+  #end
 
   super.create();
 }
 
-override public function update();
-{
+    override public function update(elapsed:Float){
+        if (controls.ACCEPT){
+            FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
+            FlxG.camera.flash(ClientPrefs.flashing ? FlxColor.WHITE : 0x4CFFFFFF, 1);
+			    	FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
+            MusicBeatState.switchState(new MainMenuState());
+        }
 
-  starsBG.x = FlxMath.lerp(starsBG.x, starsBG.x - 0.5, CoolUtil.boundTo(elapsed * 9, 0, 1));
+	      starsBG.x = FlxMath.lerp(starsBG.x, starsBG.x - 0.5, CoolUtil.boundTo(elapsed * 9, 0, 1));
 
-  super.update();
- }
+        super.update(elapsed);
+    }
 }
