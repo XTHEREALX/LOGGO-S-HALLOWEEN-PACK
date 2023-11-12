1845cec53d60f0284b5c6b195dc2819e1b3247af
XTHEREALX <149897908+XTHEREALX@users.noreply.github.com>
2023-11-11 21:25:34 -0300

    Update NewTitleState.hx

--- source/NewTitleState.hx
+++ source/NewTitleState.hx
@@ -26,7 +26,7 @@ var categoryIcon:FlxSprite;
         starsBG.scrollFactor.set();
         add(starsBG);
 
-	BG = new FlxSprite().loadGraphic(Paths.image('SelectMenu/Border'));
+	BG = new FlxSprite().loadGraphic(Paths.image('TitleScreen'));
 	BG.screenCenter();
 	add(BG);
 
@@ -44,37 +44,9 @@ var categoryIcon:FlxSprite;
     }
 
     override public function update(elapsed:Float){
-        
-		if (controls.UI_LEFT_P) 
-			changeSelection(-1);
-		if (controls.UI_RIGHT_P) 
-			changeSelection(1);
-		if (controls.BACK) {
-			FlxG.sound.play(Paths.sound('cancelMenu'));
-			MusicBeatState.switchState(new MainMenuState());
-		}
-        if (controls.ACCEPT){
-            MusicBeatState.switchState(new FreeplayState());
-        }
 
 	starsBG.x = FlxMath.lerp(starsBG.x, starsBG.x - 0.5, CoolUtil.boundTo(elapsed * 9, 0, 1));
 
-        curCategory = curSelected;
-
         super.update(elapsed);
     }
-
-    function changeSelection(change:Int = 0) {
-		curSelected += change;
-		if (curSelected < 0)
-			curSelected = freeplayCats.length - 1;
-		if (curSelected >= freeplayCats.length)
-			curSelected = 0;
-
-		var bullShit:Int = 0;
-
-		categoryIcon.loadGraphic(Paths.image('SelectMenu/Categories/logo-' + (freeplayCats[curSelected].toLowerCase())));
-	        categoryIcon.screenCenter();
-		FlxG.sound.play(Paths.sound('scrollMenu'));
-	}
 }
