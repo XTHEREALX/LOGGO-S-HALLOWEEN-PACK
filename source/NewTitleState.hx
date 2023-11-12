70f0d1f1b36cbe8a981f1a3ab2ae222d4589da6a
XTHEREALX <149897908+XTHEREALX@users.noreply.github.com>
2023-11-11 21:27:55 -0300

    Update NewTitleState.hx

--- source/NewTitleState.hx
+++ source/NewTitleState.hx
@@ -1,52 +1,60 @@
 package;
 
-import flixel.group.FlxGroup.FlxTypedGroup;
-import flixel.addons.display.FlxBackdrop;
-import flixel.FlxSprite;
-import flixel.math.FlxMath;
+import flash.text.TextField;
 import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.addons.display.FlxBackdrop;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flixel.system.FlxSound;
 
-class NewTitleState extends MusicBeatState{
-public static var freeplayCats:Array<String> = ['loggodx', 'loggoplus', 'loggodside', 'loggob3', 'loggoneo', 'loggoObjects', 'loggoneo'];
-public static var curCategory:Int = 0;
-var grpCats:FlxTypedGroup<Alphabet>;
-var curSelected:Int = 0;
-var starsBG:FlxBackdrop;
-var BG:FlxSprite;
-var categoryIcon:FlxSprite;
-    override function create(){
+using StringTools;
 
-	Paths.clearStoredMemory();
-	Paths.clearUnusedMemory();
+class NewTitleState extends MusicBeatState
+{
+  var bg:FlxSprite;
+  var starsBG:FlxBackdrop;
+
+  override public function create()
+  {
+    Paths.clearStoredMemory();
+    Paths.clearUnusedMemory();
+
+    starsBG = new FlxBackdrop(Paths.image('starsBG'), 1, 1, true, true);
+    starsBG.setPosition(111.3, 67.95);
+    starsBG.updateHitbox();
+    starsBG.scrollFactor.set();
+    add(starsBG);
+
+    var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('TitleScreen'));
+    bg.updateHitbox();
+    bg.screenCenter();
+    add(BG);
+
+    #if android
+    addVirtualPad(A_B);
+    #end
+
+    FlxG.sound.playMusic(Paths.music('freakyMenu'), 0.7);
+
+  super.create();
+}
+
+override public function update();
+{
+
+  starsBG.x = FlxMath.lerp(starsBG.x, starsBG.x - 0.5, CoolUtil.boundTo(elapsed * 9, 0, 1));
 	
-        starsBG = new FlxBackdrop(Paths.image('starsBG'), 1, 1, true, true);
-	starsBG.setPosition(111.3, 67.95);
-        starsBG.antialiasing = true;
-        starsBG.updateHitbox();
-        starsBG.scrollFactor.set();
-        add(starsBG);
+  if (controls.ACCEPT)
+      {
+	MusicBeatState.switchState(new MainMenuState());
+      }
 
-	BG = new FlxSprite().loadGraphic(Paths.image('TitleScreen'));
-	BG.screenCenter();
-	add(BG);
-
-        categoryIcon = new FlxSprite().loadGraphic(Paths.image('SelectMenu/Categories/logo-' + freeplayCats[curSelected].toLowerCase()));
-	categoryIcon.updateHitbox();
-	categoryIcon.screenCenter();
-	add(categoryIcon);
-
-	#if android
-        addVirtualPad(FULL, A_B);
-        #end
-
-        changeSelection();
-        super.create();
-    }
-
-    override public function update(elapsed:Float){
-
-	starsBG.x = FlxMath.lerp(starsBG.x, starsBG.x - 0.5, CoolUtil.boundTo(elapsed * 9, 0, 1));
-
-        super.update(elapsed);
-    }
+  super.update();
+ }
 }
